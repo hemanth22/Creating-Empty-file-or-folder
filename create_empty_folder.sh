92b64220263d74eb4a3d1af6e822118e654fda91
hemanth22 <hemanth22hemu@gmail.com>
2017-02-17 11:40:26 +0530

    Minor corrections in script and updated hint

--- create_empty_folder.sh
+++ create_empty_folder.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Hint: somefile.txt"
 read -p "Enter File name: " fil
-echo "Hint: /home/username/somepath"
+echo "Hint: /home/username/somepath/"
 read -p "Enter path to create empty file: " cc
-$(cat $fil | xargs -I {} mkdir -p $cc {})
+$(cat $fil | xargs -I {} mkdir -p $cc{})
 echo "Done"
+exit
