7b4863b10692b1eff00b3a82ba7e834a6db97077
hemanth22 <hemanth22hemu@gmail.com>
2018-01-25 17:33:08 +0530

    Adding clear logs

--- empty_file.sh
+++ empty_file.sh
@@ -7,5 +7,5 @@ echo "Initializing empty file creation"
 $(cat list.txt | xargs -I {} touch /Creating-Empty-file-or-folder/{})
 echo "Empty file creation Done"
 echo "Listing empty file created using script"
-ls -la
+ls -la *.dat
 exit
